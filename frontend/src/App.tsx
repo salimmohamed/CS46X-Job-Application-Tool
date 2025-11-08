41f11218e0ebac3a6b65e59e531e80544d91a6f6
bananaslug181 <jessicafoley1817@live.com>
2025-11-07 23:10:17 -0800

    Added super bare bones upload resume page with upload/cancel button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Hello World</h2>
+      <Link to="/upload">Upload Your Resume</Link>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
