53e20ad68aaa258c7c80599bed8afad3ca96379e
Drmsay <zacharyjonespm@gmail.com>
2025-11-10 12:05:10 -0300

    updated landing page, added API resume service to send file to back-end, and updated UploadPage to use it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
 import UploadPage from './UploadPage'
 import './index.css'
 
@@ -9,8 +8,7 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/" element={<UploadPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
