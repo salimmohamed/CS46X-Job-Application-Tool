8b92286c80393cc2d7237464bd5e28eb175f2824
Drmsay <zacharyjonespm@gmail.com>
2026-01-29 18:36:00 -0800

    Build form populated with unknown candidate details from resume parsing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UploadPage from './UploadPage'
+import CandidateDetailsPage from './CandidateDetailsPage'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UploadPage />} />
+        <Route path="/candidate-details" element={<CandidateDetailsPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
